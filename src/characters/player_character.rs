d6962ef6ddd88916b9b0d49eaf08587b0090f5f4
KittyNekoMeow <157736890+KittyNekoMeow@users.noreply.github.com>
2024-04-21 18:13:56 -0700

    New way to get player stats, able to update player stats
    Got rid of get_player_stats and converted it's functionality into multiple methods. Also added methods to add or subtract from stats.

--- src/characters/player_character.rs
+++ src/characters/player_character.rs
@@ -12,4 +12,59 @@ impl PlayerCharacter {
             stats: (1, 50, 5, 5, 5)
         }
     }
+    pub fn update_level(mut self, operation: &str, num: u16) -> Self {
+        if operation == "+" {
+            self.stats.0 = self.stats.0 + num
+        } else if operation == "-" {
+            self.stats.0 = self.stats.0 - num
+        }
+        self
+    }
+    pub fn update_health(mut self, operation: &str, num: u16) -> Self {
+        if operation == "+" {
+            self.stats.1 += num
+        } else if operation == "-" {
+            self.stats.1 -= num
+        }
+        self
+    }
+    pub fn update_vit(mut self, operation: &str, num: u16) -> Self {
+        if operation == "+" {
+            self.stats.2 = self.stats.2 + num
+        } else if operation == "-" {
+            self.stats.2 = self.stats.2 - num
+        }
+        self
+    }
+    pub fn update_int(mut self, operation: &str, num: u16) -> Self {
+        if operation == "+" {
+            self.stats.3 = self.stats.3 + num
+        } else if operation == "-" {
+            self.stats.3 = self.stats.3 - num
+        }
+        self
+    }
+    pub fn update_speed(mut self, operation: &str, num: u16) -> Self {
+        if operation == "+" {
+            self.stats.4 = self.stats.4 + num
+        } else if operation == "-" {
+            self.stats.4 = self.stats.4 - num
+        }
+        self
+    }
+    pub fn get_level(self) -> u16 {
+        self.stats.0
+    }
+    pub fn get_health(self) -> u16 {
+        self.stats.1
+    }
+    pub fn get_vit(self) -> u16 {
+        self.stats.2
+    }
+    pub fn get_int(self) -> u16 {
+        self.stats.3
+    }
+    pub fn get_speed(self) -> u16 {
+        self.stats.4
+    }
 }
